7a546e3c947e6a338daff53e590f8d7767966f4c
TheConstantinus <konstantin-nt@bk.ru>
2023-06-01 18:14:27 +0500

    update index.js

--- index.js
+++ index.js
@@ -30,9 +30,31 @@ window.onload = () => {
     context.beginPath();
     for (let i = 0; i < mf; i++) {
       const f = flakes[i];
-      context.moveto(f.x, f.y);
+      context.moveTo(f.x, f.y);
       context.arc(f.x, f.y, f.r, 0, Math.PI * 2, true);
     }
     context.fill();
+    moveFlakes();
   };
+
+  // animate snowflakes
+  let angle = 0;
+  const moveFlakes = () => {
+    angle += 0.01;
+    for (let i = 0; i < mf; i++) {
+      // store current snowflake
+      const f = flakes[i];
+
+      // update X and Y of each snowflake
+      f.y += Math.pow(f.d, 2) + 1;
+      f.x += Math.sin(angle) * 2;
+
+      // if snowflake reaches the bottom, send a new one to the top
+      if (f.y > H) {
+        flakes[i] = { x: Math.random() * W, y: 0, r: f.r, d: f.d };
+      }
+    }
+  };
+
+  setInterval(drawFlakes, 25);
 };
